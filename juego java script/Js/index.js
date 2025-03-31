a1f16105d25e97dba8bb2413f558f2690b17a430
marcosvalbuena159 <marcos.valbuena560@gmail.com>
2025-03-31 16:31:58 -0500

    cuarta carga

--- juego java script/Js/index.js
+++ juego java script/Js/index.js
@@ -39,3 +39,56 @@ function Loguear() {
         alert("Por favor ingrese correo y contraseña");
     }
 }
+
+
+class Entrenador {
+    constructor(nombreEntrenador) {
+        this.nombreEntrenador = nombreEntrenador;
+        this.pokimons = [];
+    }
+
+    agregarPokimon(pokimon) {
+        this.pokimons.push(pokimon);
+    }
+}
+
+class Pokimon {
+    constructor(nombrePokimon, tipoPokimon, habilidad, ataque, defensa, evolucion) {
+        nombrePokimon = nombrePokimon;
+        tipoPokimon = tipoPokimon;
+        habilidad = habilidad;
+        ataque = ataque;
+        defensa = defensa;
+        evolucion = evolucion;
+    }
+
+    atacar(oponente) {
+        console.log(`${this.nombrePokimon} ataca a ${oponente.nombrePokimon} con ${this.ataque} de poder!`);
+    }
+
+    defender() {
+        console.log(`${this.nombrePokimon} se defiende con ${this.defensa} de resistencia!`);
+    }
+}
+
+class Batalla {
+    static ataque(pokimon1, pokimon2) {
+        console.log(`${pokimon1.nombrePokimon} ataca a ${pokimon2.nombrePokimon}!`);
+        let daño = pokimon1.ataque - pokimon2.defensa;
+        console.log(`El daño causado es de ${daño > 0 ? daño : 0}`);
+    }
+
+    static defensa(pokimon) {
+        console.log(`${pokimon.nombrePokimon6} se prepara para defenderse!`);
+    }
+}
+
+let pokimons = [
+    new Pokimon("Pikachu", "Eléctrico", "Electricidad Estática", 55, 40, "Raichu"),
+    new Pokimon("Charmander", "Fuego", "Mar de Llamas", 52, 43, "Charmeleon"),
+    new Pokimon("Bulbasaur", "Planta/Veneno", "Espesura", 49, 49, "Ivysaur"),
+    new Pokimon("Squirtle", "Agua", "Torrente", 48, 65, "Wartortle"),
+    new Pokimon("Eevee", "Normal", "Adaptable", 55, 50, "Varias Evoluciones")
+];
+
+
